e02b1e9a1662986b0b0c0d1ea9195adabf652cf8
Taeyoung Han <tyhan02@naver.com>
2023-06-08 09:42:35 +0900

    java-study

---
+++ App/App2.java
@@ -0,0 +1,9 @@
+public class App2 {
+    public static void main(String[] args) {
+        System.out.println("도서 관리 시스템");//
+        System.out.println("----------------------------------");
+
+        System.out.println("1.도서추가 2.도서검색 3.도서삭제 4.도서수정 5.전체도서정보출력 6.종료");
+
+    }
+}
