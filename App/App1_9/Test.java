61b970eb9e59492a1aedfbc6d072eab2fbe463e1
Taeyoung Han <tyhan02@naver.com>
2023-06-09 10:49:55 +0900

    java-study

--- App/App1_9/Test.java
+++ App/App1_9/Test.java
@@ -3,7 +3,6 @@ package App1_9;
 // 소스 코드에서 Calculator 클래스는 bitcamp.util 패키지에 소속된 클래스를 가리킨다.
 import util.Calculator;
 public class Test {
-
     public static void main(String[] args) {
         // 2 * 3 + 7 - 2 / 2 = ?
         // => 연산자 우선 순위를 고려하지 않고 앞에서부터 뒤로 순차적으로 계산한다.
@@ -16,5 +15,4 @@ public class Test {
 
         System.out.println(Calculator.result);
     }
-
 }
--- App/util/Calculator.java
+++ App/util/Calculator.java
@@ -4,23 +4,18 @@ package util;
 public class Calculator {
 
     public static int result = 0;
-
     public static void init(int a) {
         result = a;
     }
-
     public static void plus(int a) {
         result += a;
     }
-
     public static void minus(int a) {
         result -= a;
     }
-
     public static void multiple(int a) {
         result *= a;
     }
-
     public static void divide(int a) {
         result /= a;
     }
