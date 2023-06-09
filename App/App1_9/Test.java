861887b6c7b8baa8b8e4844ba0da5b3e8923977d
Taeyoung Han <tyhan02@naver.com>
2023-06-09 10:22:30 +0900

    java-study

--- App/App1_9/Test.java
+++ App/App1_9/Test.java
@@ -7,29 +7,42 @@ public class Test {
     public static void main(String[] args) {
         // 2 * 3 + 7 - 2 / 2 = ?
         // => 연산자 우선 순위를 고려하지 않고 앞에서부터 뒤로 순차적으로 계산한다.
-
         int result = 0;
 
-        result = mul(2,3);
-        result= plus(result,7);
+
+        init(2);
+        mul(3);
+        plus(7);
+        minus(2);
+
+        result = mul(2, 3);
+        result = plus(result, 7);
         result = minus(result, 2);
         result = div(result, 2);
 
         System.out.println(result);
-
     }
 
-    static int plus(int a, int b){
-        return a+b;
-    }
-    static int minus(int a, int b){
-        return a-b;
-    }
-    static int mul(int a, int b){
-        return a*b;
-    }
-    static int div(int a, int b){
-        return a/b;
+
+
+    static void init(int a) {
+        result = a;
     }
 
+    static void plus(int a) {
+        result += a
+    }
+
+    static void minus(int a) {
+        result -= a;
+    }
+
+    static int mul(int a) {
+        result *= a;
+    }
+
+    static int div(int a) {
+        result /= a;
+    }
 }
+
