fecbecf324e758036577fdbd71d40b72a7d27d37
Taeyoung Han <tyhan02@naver.com>
2023-06-09 11:29:12 +0900

    java-study

--- App/util/Calculator.java
+++ App/util/Calculator.java
@@ -2,24 +2,24 @@ package util;
 
 public class Calculator {
 
-    public static int result = 0;
+    public int result = 0;
 
-    public static int getResult(){
-        return result;
+    public static int getResult(Calculator c){
+        return c.result;
     }
-    public static void init(int a) {
-        result = a;
+    public static void init(Calculator c, int a) {
+        c.result = a;
     }
-    public static void plus(int a) {
-        result += a;
+    public static void plus(Calculator c, int a) {
+        c.result += a;
     }
-    public static void minus(int a) {
-        result -= a;
+    public static void minus(Calculator c, int a) {
+        c.result -= a;
     }
-    public static void multiple(int a) {
-        result *= a;
+    public static void multiple(Calculator c, int a) {
+        c.result *= a;
     }
-    public static void divide(int a) {
-        result /= a;
+    public static void divide(Calculator c, int a) {
+        c.result /= a;
     }
 }
