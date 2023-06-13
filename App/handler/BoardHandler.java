52d624bb9da054a91130b85785b65877426f1c0b
Taeyoung Han <tyhan02@naver.com>
2023-06-13 19:08:26 +0900

    java-study

--- App/handler/BoardHandler.java
+++ App/handler/BoardHandler.java
@@ -1,5 +1,6 @@
 package handler;
 
+import util.Board;
 import util.Prompt;
 
 import java.util.Date;
@@ -36,8 +37,6 @@ public class BoardHandler {
             Board board = boards[i];
         }
 
-        Date date = new Date((board.getCreatedDate()));
-
         for (int i = 0; i < length; i++) {
             Board board = boards[i];
             System.out.printf("%d, %s, %s, %d, %tY-%5$tm-%5$td\n",
@@ -52,44 +51,45 @@ public class BoardHandler {
 
 
     public static void viewBoard() {
-        String memberNo = Prompt.inputString("번호? ");
+        String boardNo = Prompt.inputString("번호? ");
         for (int i = 0; i < length; i++) {
             Board board = boards[i];
-            if (board.getNo() == Integer.parseInt(memberNo)) {
-                System.out.printf("이름: %s\n", board.getName(Prompt.inputString("이름?")));
-                System.out.printf("이메일: %s\n", board.getEmail());
+            if (board.getNo() == Integer.parseInt(boardNo)) {
+                System.out.printf("제목: %s\n", board.getTitle());
+                System.out.printf("내용: %s\n", board.getContent());
+                System.out.printf("작성자: %s\n", board.getWriter());
 
                 return;
             }
         }
-        System.out.println("해당 번호의 회원이 없습니다!");
+        System.out.println("해당 번호의 게시글이 없습니다!");
     }
     
 
     public static void updateBoard() {
-        String memberNo = Prompt.inputString("번호? ");
+        String boardNo = Prompt.inputString("번호? ");
         for (int i = 0; i < length; i++) {
             Board board = boards[i];
-            if (board.getNo() == Integer.parseInt(memberNo)) {
-                System.out.printf("이름(%s)? ", board.getName(Prompt.inputString("이름?")));
-                board.setName(Prompt.inputString(""));
-                System.out.printf("이메일(%s)? ", board.getEmail());
-                board.setEmail(Prompt.inputString(""));
+            if (board.getNo() == Integer.parseInt(boardNo)) {
+                System.out.printf("이름(%s)? ", board.getTitle());
+                board.setTitle(Prompt.inputString(""));
+                System.out.printf("작성자(%s)? ", board.getWriter());
+                board.setWriter(Prompt.inputString(""));
                 System.out.printf("새암호? ");
                 board.setPassword(Prompt.inputString(""));
                 return;
             }
         }
-        System.out.println("해당 번호의 회원이 없습니다!");
+        System.out.println("해당 번호의 게시글이 없습니다!");
     }
     
 
     public static void deleteBoard() {
-        int memberNo = Prompt.inputInt("번호? ");
+        int boardNo = Prompt.inputInt("번호? ");
 
-        int deletedIndex = indexOf(memberNo);
+        int deletedIndex = indexOf(boardNo);
         if (deletedIndex == -1) {
-            System.out.println("해당 번호의 회원이 없습니다!");
+            System.out.println("해당 번호의 게시글이 없습니다!");
             return;
         }
 
@@ -100,10 +100,10 @@ public class BoardHandler {
         boards[--length] = null;
     }
 
-    private static int indexOf(int memberNo) {
+    private static int indexOf(int boardNo) {
         for (int i = 0; i < length; i++) {
             Board board = boards[i];
-            if (board.getNo() == memberNo) {
+            if (board.getNo() == boardNo) {
                 return i;
             }
         }
