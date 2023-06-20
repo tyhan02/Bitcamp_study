24f322c3c72f6c2ecc175d7fe134005811725b4d
Taeyoung Han <tyhan02@naver.com>
2023-06-20 11:50:25 +0900

    Test

---
+++ Test/Test_0620/Member.java
@@ -0,0 +1,8 @@
+package Test_0620;
+
+public class Member {
+
+    String name;
+    String email;
+    String password;
+}
---
+++ Test/Test_0620/Student.java
@@ -0,0 +1,10 @@
+package Test_0620;
+
+import handler.BoardList;
+
+public class Student extends Member{
+
+    String school;
+    String major;
+    boolean working;
+}
