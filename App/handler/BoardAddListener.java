78a9b3ef97a23806ee95ec09311482229a7bd34c
Taeyoung Han <tyhan02@naver.com>
2023-06-21 15:45:01 +0900

    regactoring

---
+++ App/handler/BoardAddListener.java
@@ -0,0 +1,26 @@
+package handler;
+
+
+import util.ActionListener;
+import util.BreadcrumbPrompt;
+import util.List;
+
+public class BoardAddListener implements ActionListener {
+
+    private List list;
+
+    public BoardAddListener(List list) {
+        this.list = list;
+    }
+
+    @Override
+    public void service(BreadcrumbPrompt prompt) {
+        Board board = new Board();
+        board.setTitle(prompt.inputString("제목? "));
+        board.setContent(prompt.inputString("내용? "));
+        board.setWriter(prompt.inputString("작성자? "));
+        board.setPassword(prompt.inputString("암호? "));
+        this.list.add(board);
+    }
+}
+
