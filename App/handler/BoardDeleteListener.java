f8a5289620f3e657eca0610ba87f2283f7fb9114
Taeyoung Han <tyhan02@naver.com>
2023-06-21 15:45:15 +0900

    regactoring

---
+++ App/handler/BoardDeleteListener.java
@@ -0,0 +1,24 @@
+package handler;
+
+
+import util.ActionListener;
+import util.BreadcrumbPrompt;
+import util.List;
+
+
+public class BoardDeleteListener implements ActionListener {
+
+    private List list;
+
+    public BoardDeleteListener(List list) {
+        this.list = list;
+    }
+
+    @Override
+    public void service(BreadcrumbPrompt prompt) {
+        if (!this.list.remove(new Board(prompt.inputInt("번호? ")))) {
+            System.out.println("해당 번호의 게시글이 없습니다!");
+        }
+    }
+}
+
