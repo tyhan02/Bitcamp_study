1f331952a3ee719337c2de833d8b562e826d4947
Taeyoung Han <tyhan02@naver.com>
2023-06-22 12:46:46 +0900

    try-catch

--- Test/Test_0622/Exam01.java
+++ Test/Test_0622/Exam01.java
@@ -1,11 +1,22 @@
 package Test_0622;
 
 class A{
-    static int v1; //스태틱변수는 클래스 로딩될 때
-    static void m1() {}
+
+    static int v1=111; //스태틱변수는 클래스 로딩될 때
     static {
+        v1=100;
         System.out.println("A클래스의 스태틱 블록 실행");
     }
+    static {
+        v1=200;
+        System.out.println("A클래스의 스태틱 블록 실행2");
+    }
+    static {
+        v1=300;
+        System.out.println("A클래스의 스태틱 블록 실행3");
+    }
+    static int v2=222;
+    static void m1(){}
 }
 public class Exam01 {
     public static void main(String[] args) {
