3c7f4f826ad814221d9f6db0f971c913f72dd849
Taeyoung Han <tyhan02@naver.com>
2023-06-26 11:52:29 +0900

    generic

--- App/handler/AbstractMemberListener.java
+++ App/handler/AbstractMemberListener.java
@@ -6,9 +6,9 @@ import util.List;
 
 public abstract class AbstractMemberListener implements ActionListener {
 
-    protected List list;
+    protected List<Member> list;
 
-    public AbstractMemberListener(List list) {
+    public AbstractMemberListener(List<Member> list) {
         this.list = list;
     }
 
@@ -18,7 +18,7 @@ public abstract class AbstractMemberListener implements ActionListener {
 
     protected Member findBy(int no) {
         for (int i = 0; i < this.list.size(); i++) {
-            Member m = (Member) this.list.get(i);
+            Member m = this.list.get(i);
             if (m.getNo() == no) {
                 return m;
             }
