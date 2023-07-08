dd69eb2958019550a779515d5350f3ddbae04d1c
Taeyoung Han <tyhan02@naver.com>
2023-07-08 16:45:46 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -46,7 +46,7 @@ public class App {
 
   static void printTitle() {
     System.out.println("나의 목록 관리 시스템");
-    System.out.println("----------------------------------");
+    System.out.println("----------------------------------*");
   }
 
   public void execute() {
