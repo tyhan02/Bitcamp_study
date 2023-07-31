9bebf75c4ee1c1d796a6576b49c43f38185b416b
Taeyoung Han <tyhan02@naver.com>
2023-07-31 16:29:11 +0900

    update

---
+++ HW_Project/Project11/AuthenException.java
@@ -0,0 +1,12 @@
+package Project11;
+
+public class AuthenException extends Exception{
+
+    private static final long serialVersionUID = 1L;
+
+    public AuthenException(String message){
+        super(message);
+    }
+
+
+}
