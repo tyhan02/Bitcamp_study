1691570f7b265e49fb4221bb81fb1359c0cc3ce2
Taeyoung Han <tyhan02@naver.com>
2023-07-31 16:29:47 +0900

    update

---
+++ HW_Project/Project11/Picture.java
@@ -0,0 +1,18 @@
+package Project11;
+
+
+public class Picture {
+    public void main1() {
+
+
+        System.out.println("GO");
+
+
+    }
+    public void main2(){
+
+
+        System.out.println("BYE");
+
+    }
+}
