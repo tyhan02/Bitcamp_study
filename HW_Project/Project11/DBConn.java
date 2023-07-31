394677baddc5be324dfe93849aeb8dbfe507c978
Taeyoung Han <tyhan02@naver.com>
2023-07-31 16:29:18 +0900

    update

---
+++ HW_Project/Project11/DBConn.java
@@ -0,0 +1,42 @@
+package Project11;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConn {
+
+    private static Connection dbConn;
+
+    public static Connection getConnection() {
+
+        if (dbConn == null) {
+            try {
+                // MySQL JDBC URL에 맞게 수정
+                String url = "jdbc:mysql://localhost:3306/money";
+                String user = "root"; // MySQL 계정 사용자명
+                String password = "0000"; // MySQL 계정 비밀번호
+
+                dbConn = DriverManager.getConnection(url, user, password);
+
+            } catch (SQLException e) {
+                System.out.println(e.toString());
+            }
+        }
+        return dbConn;
+    }
+
+    public static void close() {
+
+        if (dbConn != null) {
+            try {
+                if (!dbConn.isClosed()) {
+                    dbConn.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.toString());
+            }
+        }
+        dbConn = null;
+    }
+}
