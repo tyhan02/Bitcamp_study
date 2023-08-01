ecf4485a9fac07d6f550e3e9d8777422fe55a73c
Taeyoung Han <tyhan02@naver.com>
2023-08-01 14:42:59 +0900

    update

--- myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -1,68 +1,30 @@
 package bitcamp.myapp;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import bitcamp.myapp.dao.BoardDao;
-import bitcamp.myapp.dao.MemberDao;
-import bitcamp.myapp.dao.MySQLBoardDao;
-import bitcamp.myapp.dao.MySQLMemberDao;
-import bitcamp.myapp.handler.BoardAddListener;
-import bitcamp.myapp.handler.BoardDeleteListener;
-import bitcamp.myapp.handler.BoardDetailListener;
-import bitcamp.myapp.handler.BoardListListener;
-import bitcamp.myapp.handler.BoardUpdateListener;
-import bitcamp.myapp.handler.LoginListener;
-import bitcamp.myapp.handler.MemberAddListener;
-import bitcamp.myapp.handler.MemberDeleteListener;
-import bitcamp.myapp.handler.MemberDetailListener;
-import bitcamp.myapp.handler.MemberListListener;
-import bitcamp.myapp.handler.MemberUpdateListener;
-import bitcamp.net.NetProtocol;
-import bitcamp.util.BreadcrumbPrompt;
-import bitcamp.util.Menu;
-import bitcamp.util.MenuGroup;
+import bitcamp.util.ApplicationContext;
+import bitcamp.util.DispatcherListener;
 import bitcamp.util.SqlSessionFactoryProxy;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class ServerApp {
 
-  // 자바 스레드풀 준비
-  ExecutorService threadPool = Executors.newFixedThreadPool(2);
-
-  SqlSessionFactory sqlSessionFactory;
-
-  MemberDao memberDao;
-  BoardDao boardDao;
-
-  MenuGroup mainMenu = new MenuGroup("메인");
+  ApplicationContext iocContainer;
+  DispatcherListener facadeListener;
 
   int port;
 
   public ServerApp(int port) throws Exception {
-
     this.port = port;
-
-    // 1) mybatis 설정 파일을 읽어들일 도구를 준비한다.
-    InputStream mybatisConfigIn = Resources.getResourceAsStream("bitcamp/myapp/config/mybatis-config.xml");
-
-    // 2) SqlSessionFactory를 만들어줄 빌더 객체 준비
-    SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-
-    // 3) 빌더 객체를 통해 SqlSessionFactory를 생성
-    sqlSessionFactory = new SqlSessionFactoryProxy(builder.build(mybatisConfigIn));
-
-    this.memberDao = new MySQLMemberDao(sqlSessionFactory);
-    this.boardDao = new MySQLBoardDao(sqlSessionFactory);
-
-    prepareMenu();
+    //    iocContainer = new ApplicationContext(AppConfig.class);
+    //    facadeListener = new DispatcherListener(iocContainer);
   }
 
   public void close() throws Exception {
@@ -75,76 +37,70 @@ public class ServerApp {
     app.close();
   }
 
-  public void execute() {
-    try (ServerSocket serverSocket = new ServerSocket(this.port)) {
-      System.out.println("서버 실행 중...");
+  public void execute() throws Exception {
+    // ServerSocket을 통해 클라이언트 연결을 처리하는 스레드 그룹
+    EventLoopGroup bossGroup = new NioEventLoopGroup();
 
-      while (true) {
-        Socket socket = serverSocket.accept();
-        threadPool.execute(() -> processRequest(socket));
-      }
-    } catch (Exception e) {
-      System.out.println("서버 실행 오류!");
-      e.printStackTrace();
+    // 클라이언트와 연결된 Socket의 통신을 처리하는 스레드 그룹
+    EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+    try {
+      // 서버 설정을 수행할 객체 준비
+      ServerBootstrap bootstrap = new ServerBootstrap();
+
+      // 서버로서 작업을 수행할 스레드 그룹을 설정
+      bootstrap.group(bossGroup, workerGroup);
+
+      // 클라이언트와 통신하는 채널의 타입을 지정
+      // 클라이언트가 연결되면 이 타입의 객체를 생성한다.
+      // 이 채널 객체가 클라이언트와의 통신을 수행한다.
+      bootstrap.channel(NioServerSocketChannel.class);
+
+      // 클라이언트와 연결된 채널을 준비시키는 객체 설정
+      bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+        @Override
+        public void initChannel(SocketChannel ch) throws Exception {
+          // 클라이언트가 연결되면 이 메서드가 호출되어
+          // 통신 채널을 준비한다.
+          // => 클라이언트가 보낸 요청을 처리할 객체 등록
+          ch.pipeline().addLast(new ServerHandler());
+        }
+      });
+
+      // 대기열 개수 설정
+      bootstrap.option(ChannelOption.SO_BACKLOG, 128);
+
+      // 클라이언트의 연결을 계속 유지할 것인지 설정
+      bootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
+
+      System.out.println("ServerBootstrap 설정 완료!");
+
+      // 설정된 정보를 기반으로 서버 시작
+      ChannelFuture f = bootstrap.bind(port).sync();
+      System.out.println("서버 시작됨!");
+
+      // 모든 클라이언트의 연결이 끊어지면 종료하라고 예약한다.
+      // => 서버가 종료할 때까지 리턴되지 않는다.
+      f.channel().closeFuture().sync();
+      System.out.println("서버 종료됨!");
+
+    } finally {
+      workerGroup.shutdownGracefully();
+      bossGroup.shutdownGracefully();
     }
   }
 
   private void processRequest(Socket socket) {
-    try (Socket s = socket;
-         DataInputStream in = new DataInputStream(socket.getInputStream());
-         DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
-
-      BreadcrumbPrompt prompt = new BreadcrumbPrompt(in, out);
-
-      InetSocketAddress clientAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
-      System.out.printf("%s 클라이언트 접속함!\n", clientAddress.getHostString());
-
-      out.writeUTF("[나의 목록 관리 시스템]\n"
-              + "-----------------------------------------");
-
-      new LoginListener(memberDao).service(prompt);
-
-      mainMenu.execute(prompt);
-      out.writeUTF(NetProtocol.NET_END);
+    try {
 
     } catch (Exception e) {
       System.out.println("클라이언트 통신 오류!");
       e.printStackTrace();
 
     } finally {
-      ((SqlSessionFactoryProxy) sqlSessionFactory).clean();
+      SqlSessionFactoryProxy sqlSessionFactoryProxy =
+              (SqlSessionFactoryProxy) iocContainer.getBean(SqlSessionFactory.class);
+      sqlSessionFactoryProxy.clean();
     }
-  }
-
-  private void prepareMenu() {
-    MenuGroup memberMenu = new MenuGroup("회원");
-    memberMenu.add(new Menu("등록", new MemberAddListener(memberDao, sqlSessionFactory)));
-    memberMenu.add(new Menu("목록", new MemberListListener(memberDao)));
-    memberMenu.add(new Menu("조회", new MemberDetailListener(memberDao)));
-    memberMenu.add(new Menu("변경", new MemberUpdateListener(memberDao, sqlSessionFactory)));
-    memberMenu.add(new Menu("삭제", new MemberDeleteListener(memberDao, sqlSessionFactory)));
-    mainMenu.add(memberMenu);
-
-    MenuGroup boardMenu = new MenuGroup("게시글");
-    boardMenu.add(new Menu("등록", new BoardAddListener(1, boardDao, sqlSessionFactory)));
-    boardMenu.add(new Menu("목록", new BoardListListener(1, boardDao)));
-    boardMenu.add(new Menu("조회", new BoardDetailListener(1, boardDao, sqlSessionFactory)));
-    boardMenu.add(new Menu("변경", new BoardUpdateListener(1, boardDao, sqlSessionFactory)));
-    boardMenu.add(new Menu("삭제", new BoardDeleteListener(1, boardDao, sqlSessionFactory)));
-    mainMenu.add(boardMenu);
-
-    MenuGroup readingMenu = new MenuGroup("독서록");
-    readingMenu.add(new Menu("등록", new BoardAddListener(2, boardDao, sqlSessionFactory)));
-    readingMenu.add(new Menu("목록", new BoardListListener(2, boardDao)));
-    readingMenu.add(new Menu("조회", new BoardDetailListener(2, boardDao, sqlSessionFactory)));
-    readingMenu.add(new Menu("변경", new BoardUpdateListener(2, boardDao, sqlSessionFactory)));
-    readingMenu.add(new Menu("삭제", new BoardDeleteListener(2, boardDao, sqlSessionFactory)));
-    mainMenu.add(readingMenu);
-
-    //    Menu helloMenu = new Menu("안녕!");
-    //    helloMenu.addActionListener(new HeaderListener());
-    //    helloMenu.addActionListener(new HelloListener());
-    //    helloMenu.addActionListener(new FooterListener());
-    //    mainMenu.add(helloMenu);
   }
 }
