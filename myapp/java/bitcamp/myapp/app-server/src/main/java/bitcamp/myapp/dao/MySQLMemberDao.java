049fc303432764ac8aaf55511ea93ca78c3690ea
Taeyoung Han <tyhan02@naver.com>
2023-08-18 10:03:21 +0900

    ncp 연결

--- myapp/java/bitcamp/myapp/app-server/src/main/java/bitcamp/myapp/dao/MySQLMemberDao.java
+++ myapp/java/bitcamp/myapp/app-server/src/main/java/bitcamp/myapp/dao/MySQLMemberDao.java
@@ -1,52 +1,54 @@
-package bitcamp.myapp.dao;
+package bitcamp.myapp.handler;
 
-import java.util.List;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import bitcamp.myapp.vo.Member;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class MySQLMemberDao implements MemberDao {
+@WebServlet("/board/form")
+public class BoardFormServlet extends HttpServlet {
 
-  SqlSessionFactory sqlSessionFactory;
-
-  public MySQLMemberDao(SqlSessionFactory sqlSessionFactory) {
-    this.sqlSessionFactory = sqlSessionFactory;
-  }
-
-  @Override
-  public void insert(Member member) {
-    SqlSession sqlSession = sqlSessionFactory.openSession(false);
-    sqlSession.insert("bitcamp.myapp.dao.MemberDao.insert", member);
-  }
+  private static final long serialVersionUID = 1L;
 
   @Override
-  public List<Member> findAll() {
-    SqlSession sqlSession = sqlSessionFactory.openSession(false);
-    return sqlSession.selectList("bitcamp.myapp.dao.MemberDao.findAll");
-  }
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
 
-  @Override
-  public Member findBy(int no) {
-    SqlSession sqlSession = sqlSessionFactory.openSession(false);
-    return sqlSession.selectOne("bitcamp.myapp.dao.MemberDao.findBy", no);
-  }
+    int category = Integer.parseInt(request.getParameter("category"));
 
-  @Override
-  public Member findByEmailAndPassword(Member member) {
-    SqlSession sqlSession = sqlSessionFactory.openSession(false);
-    return sqlSession.selectOne("bitcamp.myapp.dao.MemberDao.findByEmailAndPassword", member);
-  }
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<meta charset='UTF-8'>");
+    out.println("<title>비트캠프</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>게시글</h1>");
+    out.println("<form action='/board/add' method='post' enctype='multipart/form-data'>");
+    out.println("제목 <input type='text' name='title'><br>");
+    out.println("내용 <textarea name='content'></textarea><br>");
+    out.println("파일 <input type='file' name='files' multiple><br>");
+    out.printf("<input type='hidden' name='category' value='%d'>\n", category);
+    out.println("<button>등록</button>");
+    out.println("</form>");
+    out.println("</body>");
+    out.println("</html>");
 
-  @Override
-  public int update(Member member) {
-    SqlSession sqlSession = sqlSessionFactory.openSession(false);
-    return sqlSession.update("bitcamp.myapp.dao.MemberDao.update", member);
   }
-
-  @Override
-  public int delete(int no) {
-    SqlSession sqlSession = sqlSessionFactory.openSession(false);
-    return sqlSession.delete("bitcamp.myapp.dao.MemberDao.delete", no);
-  }
-
 }
+
+
+
+
+
+
+
+
+
+
+
--- myapp/java/bitcamp/myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardDetailServlet.java
+++ myapp/java/bitcamp/myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardDetailServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import bitcamp.myapp.vo.AttachedFile;
 import bitcamp.myapp.vo.Board;
 
 @WebServlet("/board/detail")
@@ -16,11 +17,12 @@ public class BoardDetailServlet extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+          throws ServletException, IOException {
 
-    Board board = InitServlet.boardDao.findBy(
-        Integer.parseInt(request.getParameter("category")),
-        Integer.parseInt(request.getParameter("no")));
+    int category = Integer.parseInt(request.getParameter("category"));
+    int no = Integer.parseInt(request.getParameter("no"));
+
+    Board board = InitServlet.boardDao.findBy(category, no);
 
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
@@ -37,25 +39,37 @@ public class BoardDetailServlet extends HttpServlet {
       out.println("<p>해당 번호의 게시글이 없습니다!</p>");
 
     } else {
-      out.println("<form action='/board/update' method='post'>");
+      out.println("<form action='/board/update' method='post' enctype='multipart/form-data'>");
       out.printf("<input type='hidden' name='category' value='%d'>\n", board.getCategory());
       out.println("<table border='1'>");
       out.printf("<tr><th style='width:120px;'>번호</th>"
-          + " <td style='width:300px;'><input type='text' name='no' value='%d' readonly></td></tr>\n", board.getNo());
+              + " <td style='width:300px;'><input type='text' name='no' value='%d' readonly></td></tr>\n", board.getNo());
       out.printf("<tr><th>제목</th>"
-          + " <td><input type='text' name='title' value='%s'></td></tr>\n", board.getTitle());
+              + " <td><input type='text' name='title' value='%s'></td></tr>\n", board.getTitle());
       out.printf("<tr><th>내용</th>"
-          + " <td><textarea name='content' style='height:200px; width:400px;'>%s</textarea></td></tr>\n", board.getContent());
+              + " <td><textarea name='content' style='height:200px; width:400px;'>%s</textarea></td></tr>\n", board.getContent());
       out.printf("<tr><th>작성자</th> <td>%s</td></tr>\n", board.getWriter().getName());
       out.printf("<tr><th>조회수</th> <td>%s</td></tr>\n", board.getViewCount());
       out.printf("<tr><th>등록일</th> <td>%tY-%1$tm-%1$td</td></tr>\n", board.getCreatedDate());
+      out.println("<tr><th>첨부파일</th><td>");
+
+      for (AttachedFile file : board.getAttachedFiles()) {
+        out.printf("<a href='https://kr.object.ncloudstorage.com/bitcamp-nc7-bucket-21/board/%s'>%1$s</a>"
+                + " [<a href='/board/file/delete?category=%d&no=%d'>삭제</a>]"
+                + "<br>\n", file.getFilePath(), category, file.getNo());
+      }
+
+      out.println("<input type='file' name='files' multiple>");
+
+      out.println("</td></tr>");
       out.println("</table>");
+
 
       out.println("<div>");
       out.println("<button>변경</button>");
       out.println("<button type='reset'>초기화</button>");
       out.printf("<a href='/board/delete?category=%d&no=%d'>삭제</a>\n",
-          board.getCategory(), board.getNo());
+              board.getCategory(), board.getNo());
       out.printf("<a href='/board/list?category=%d'>목록</a>\n", board.getCategory());
       out.println("</div>");
       out.println("</form>");
