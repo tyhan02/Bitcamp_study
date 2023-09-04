e02c8950aa2a733424003bc588b4d18212212467
Taeyoung Han <tyhan02@naver.com>
2023-09-04 18:03:02 +0900

    스프링으로 가기전

--- myapp/java/bitcamp/myapp/app-server/src/main/java/bitcamp/myapp/controller/BoardFileDeleteController.java
+++ myapp/java/bitcamp/myapp/app-server/src/main/java/bitcamp/myapp/controller/BoardFileDeleteController.java
@@ -1,59 +1,48 @@
 package bitcamp.myapp.controller;
 
-import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.service.BoardService;
 import bitcamp.myapp.vo.AttachedFile;
 import bitcamp.myapp.vo.Board;
 import bitcamp.myapp.vo.Member;
-import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet("/board/fileDelete")
-public class BoardFileDeleteController extends HttpServlet {
+@Controller("/board/fileDelete")
+public class BoardFileDeleteController implements PageController {
 
-  private static final long serialVersionUID = 1L;
+  @Autowired
+  BoardService boardService;
 
   @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
+  public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
     Member loginUser = (Member) request.getSession().getAttribute("loginUser");
     if (loginUser == null) {
-      request.setAttribute("viewUrl", "redirect:../auth/login");
-      return;
+      return "redirect:../auth/login";
     }
-
-    BoardDao boardDao = (BoardDao) this.getServletContext().getAttribute("boardDao");
-    SqlSessionFactory sqlSessionFactory = (SqlSessionFactory) this.getServletContext().getAttribute("sqlSessionFactory");
 
     Board board = null;
     try {
-      int category = Integer.parseInt(request.getParameter("category"));
       int fileNo = Integer.parseInt(request.getParameter("no"));
 
-      AttachedFile attachedFile = boardDao.findFileBy(fileNo);
-      board = boardDao.findBy(category, attachedFile.getBoardNo());
+      AttachedFile attachedFile = boardService.getAttachedFile(fileNo);
+      board = boardService.get(attachedFile.getBoardNo());
       if (board.getWriter().getNo() != loginUser.getNo()) {
         throw new Exception("게시글 변경 권한이 없습니다!");
       }
 
-      if (boardDao.deleteFile(fileNo) == 0) {
-        throw new Exception("해당 번호의 첨부파일이 없거나 삭제 권한이 없습니다.");
+      if (boardService.deleteAttachedFile(fileNo) == 0) {
+        throw new Exception("해당 번호의 첨부파일이 없다.");
       } else {
-        sqlSessionFactory.openSession(false).commit();
-        request.setAttribute("viewUrl", "redirect:detail?category=" + category + "&no=" + board.getNo());
+        return "redirect:detail?no=" + board.getNo();
       }
 
     } catch (Exception e) {
-      sqlSessionFactory.openSession(false).rollback();
-      request.setAttribute("refresh", "2;url=detail?category=" + request.getParameter("category") +
-              "&no=" + board.getNo());
-      request.setAttribute("exception", e);
+      request.setAttribute("refresh", "2;url=detail?no=" + board.getNo());
+      throw e;
     }
   }
 }
