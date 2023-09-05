a800145291a888543f9541479324f499e5b477a2
Taeyoung Han <tyhan02@naver.com>
2023-09-05 17:35:05 +0900

    ->spring

--- myapp/java/bitcamp/myapp/app-common/src/main/java/bitcamp/myapp/vo/AttachedFile.java
+++ myapp/java/bitcamp/myapp/app-common/src/main/java/bitcamp/myapp/vo/AttachedFile.java
@@ -1,10 +1,8 @@
 package bitcamp.myapp.vo;
 
-import java.io.Serial;
 import java.io.Serializable;
 
 public class AttachedFile implements Serializable {
-    @Serial
     private static final long serialVersionUID = 1L;
 
     int no;
