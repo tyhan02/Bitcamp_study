1349f69082df0fdef9696472bf0a11fcd2f422b5
Taeyoung Han <tyhan02@naver.com>
2023-09-05 17:35:15 +0900

    ->spring

--- myapp/java/bitcamp/myapp/app-server/src/main/java/bitcamp/myapp/controller/HomeController.java
+++ myapp/java/bitcamp/myapp/app-server/src/main/java/bitcamp/myapp/controller/HomeController.java
@@ -1,20 +1,11 @@
 package bitcamp.myapp.controller;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.stereotype.Controller;
 
-@WebServlet("/index.jsp")
-public class HomeController extends HttpServlet {
-  private static final long serialVersionUID = 1L;
-
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    response.setContentType("text/html;charset=UTF-8");
-    request.getRequestDispatcher("/WEB-INF/jsp/index.jsp").include(request, response);
+@Controller
+public class HomeController {
+  @RequestMapping("/")
+  public String home() throws Exception {
+    return "/WEB-INF/jsp/index.jsp";
   }
 }
