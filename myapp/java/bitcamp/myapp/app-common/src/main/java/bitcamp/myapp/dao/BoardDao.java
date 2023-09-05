8d48a1e840aefa819fcc1c64b9d2c5880311a6b8
Taeyoung Han <tyhan02@naver.com>
2023-09-05 17:35:24 +0900

    ->spring

--- myapp/java/bitcamp/myapp/app-common/src/main/java/bitcamp/myapp/dao/BoardDao.java
+++ myapp/java/bitcamp/myapp/app-common/src/main/java/bitcamp/myapp/dao/BoardDao.java
@@ -1,18 +1,20 @@
 package bitcamp.myapp.dao;
 
-import java.util.List;
 import bitcamp.myapp.vo.AttachedFile;
 import bitcamp.myapp.vo.Board;
 
+import java.util.List;
+
 public interface BoardDao {
-  void insert(Board board);
+  int insert(Board board);
   List<Board> findAll(int category);
-  Board findBy(int category, int no);
+  Board findBy(int no);
   int update(Board board);
-  int updateCount(Board board);
-  int delete(Board board);
+  int updateCount(int no);
+  int delete(int no);
 
   int insertFiles(Board board);
   AttachedFile findFileBy(int no);
   int deleteFile(int fileNo);
+  int deleteFiles(int boardNo);
 }
